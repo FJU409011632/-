341e15e6a6452772858e0ba6e46d83c9d6ae9b98
FJU409011632 <166680169+FJU409011632@users.noreply.github.com>
2024-06-02 20:09:28 +0800

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#課堂練習1寫程式計算 1 ~ 100 中的所有奇數和
+s=0
+for i in range(1,100+1):
+    if i%2==0:
+        i==i
+    else:
+        s=s+i
+        #print(i,s)
+print(s)
+
---
+++ 3.2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(1,9+1):
+    for ii in range(1,9+1):
+        print(i,"x",ii,"=",i*ii)
+
+
+# In[ ]:
+
+
+
+
